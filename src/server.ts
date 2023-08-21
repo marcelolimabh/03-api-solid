beb8312519dd57876b153d65280cf33e6db75d0a
Marcelo Bicalho <marcelolimabh@gmail.com>
2023-08-21 14:32:05 -0300

    Prisma e Docker

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app'
 import { env } from './env'
-app.listen({
+app
+  .listen({
     host: '0.0.0.0',
-    port: env.PORT 
-}).then(() => {
-    console.log( '🚀 HTTP Server Running...')
-})
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🚀 HTTP Server Running...')
+  })
