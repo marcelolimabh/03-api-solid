ce88aead4b433275d48b92a7bf4f5cc48de1907a
Marcelo Bicalho <marcelolimabh@gmail.com>
2023-08-21 18:26:24 -0300

    criando controller e arquivo de rotas

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,6 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { z } from 'zod'
-import { prisma } from './lib/prisma'
+import { appRoutes } from '@/http/routes'
 
 export const app = fastify()
 
-app.post('/users', async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
-  const hash = crypto.createHash('sha512')
-
-  const { name, email, password } = registerBodySchema.parse(request.body)
-  const passwordHash = hash.update(password).digest('hex')
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: passwordHash,
-    },
-  })
-  return reply.status(201).send()
-})
+app.register(appRoutes)
