22bdd5155298a6b70ccc64ab3c60ae2d8226106a
Marcelo Bicalho <marcelolimabh@gmail.com>
2023-08-21 17:22:09 -0300

    create relations in check-in

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 
 import { PrismaClient } from '@prisma/client'
 export const app = fastify()
 
 const prisma = new PrismaClient()
+const hash = crypto.createHash('sha256')
+const password = hash.update('1234').digest('hex')
+console.log(password)
+/*
+const us = prisma.user
+  .create({
+    data: {
+      name: 'Marcelo Lima Bicalho',
+      email: 'marcelo@gmail.com',
+      password_hash: password,
+    },
+  })
+  .then(() => {
+    console.log(us)
+  })
 
-prisma.user.create({
-  data: {
-    name: 'Marcelo Lima Bicalho',
-    email: 'marcelo@gmail.com',
-  },
-})
+console.log(us)
+*/
