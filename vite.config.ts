ee8f1ca5f9063d621b279ca2ed0019389a34a05f
Marcelo Bicalho <marcelolimabh@gmail.com>
2023-09-03 14:12:13 -0300

    Controllers & Testes E2E  Criando test environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import tsconfigPaths from 'vitest-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    dir: 'src',
+  },
 })
